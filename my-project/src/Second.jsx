d7e586502ed4faeb239d1a3efe0404e50be87ad3
githoboman <smithmar681@gmail.com>
2024-08-23 15:49:58 +0100

    grid com

--- my-project/src/Second.jsx
+++ my-project/src/Second.jsx
@@ -1,4 +1,4 @@
-import image 
+
 
 
 
@@ -8,7 +8,15 @@ function Second  () {
         <h1>
             Top restaurants and more in Nigeria
         </h1>
-        img
+        <li><img src="my-project/src/assets/kfc.jpg" alt="kfc-pic" /></li>
+        <li><img src="my-project/src/assets/mamass.jpg" alt="pink" /></li>
+        <li><img src="my-project/src/assets/place.jpg" alt="plce" /></li>
+        <li><img src="my-project/src/assets/sweet.jpg" alt="sensation" /></li>
+        <li><img src="my-project/src/assets/smmalchop.jpg" alt="kitchen" /></li>
+        <li><img src="my-project/src/assets/chop now.jpg" alt="house" /></li>
+        <li><img src="my-project/src/assets/debonair.jpg" alt="me" /></li>
+        <li><img src="my-project/src/assets/fkitch.jpg" alt="letsg0" /></li>
+        
         
         
         
