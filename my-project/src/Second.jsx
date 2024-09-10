1424cb1e1be49732cc39ba61106ac55d3b337e2d
githoboman <smithmar681@gmail.com>
2024-09-10 16:20:26 +0100

    copyimg

--- my-project/src/Second.jsx
+++ my-project/src/Second.jsx
@@ -9,14 +9,14 @@ function Second  () {
             Top restaurants and more in Nigeria
         </h1>
         <ul>
-        <li><img src="my-project/src/assets/kfc.jpg" alt="kfc-pic" /></li>
-        <li><img src="my-project/src/assets/mamass.jpg" alt="pink" /></li>
-        <li><img src="my-project/src/assets/place.jpg" alt="plce" /></li>
-        <li><img src="my-project/src/assets/sweet.jpg" alt="sensation" /></li>
-        <li><img src="my-project/src/assets/smmalchop.jpg" alt="kitchen" /></li>
-        <li><img src="my-project/src/assets/chop now.jpg" alt="house" /></li>
-        <li><img src="my-project/src/assets/debonair.jpg" alt="me" /></li>
-        <li><img src="my-project/src/assets/fkitch.jpg" alt="letsg0" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/o57akgxyabkgo04kjlbu?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="kfc-pic" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/egmjz07ibkbr9uopccsc?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="pink" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/hlwqevdvj0cfzss5gpoi?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="plce" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/jui8atcdisz1fyj8xqif?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="sensation" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/zdpfnwewlbzc40surjgw?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="kitchen" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/e0pcegpluhtq0akciakd?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="house" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/cg4sdskoaecoxhp7lhkt?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="me" /></li>
+        <li><img src="https://glovo.dhmedia.io/image/customer-assets-glovo/countries/Stores/nem66tghxmqigr2fodaq?t=W3siYXV0byI6eyJxIjoibG93In19XQ==" alt="letsg0" /></li>
         
         </ul>
         
